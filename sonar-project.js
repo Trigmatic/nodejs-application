2689120fb1244dd0ee4eb0fb91b85a64d7c5297a
Abdulmajid Osumanu <abdulmajid@aims.edu.gh>
2022-07-06 00:03:19 -0400

    Update sonar-project.js

--- sonar-project.js
+++ sonar-project.js
@@ -1,7 +1,7 @@
 const sonarqubeScanner = require('sonarqube-scanner');
 
 sonarqubeScanner({
-  serverUrl: 'http://3.99.224.76:9000/',
+  serverUrl: 'http://3.96.135.191:9000/',
        options : {
 	    'sonar.projectDescription': 'This is a Node JS application',
 	    'sonar.projectName': 'Node JS Application - Sample',
